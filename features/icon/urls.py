a7f2a960d155f66ffd24a9c9b17fd897665d7193
Dhanvi Desai <dhanvi.desai@tarento.com>
2025-08-17 20:08:31 +0530

    fix: upload to S3 bucket

--- features/icon/urls.py
+++ features/icon/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import IconViewSet
 
 router = DefaultRouter()
-router.register(r'icons', IconViewSet)
+router.register(r'', IconViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
