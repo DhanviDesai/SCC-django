bf88797f3557b1e9f35aeafa53330dbebd4958b6
Dhanvi Desai <dhanvi.desai@tarento.com>
2025-08-17 21:53:31 +0530

    fix: replace slug to uuid in urls

--- features/season/urls.py
+++ features/season/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("list", views.ListSeason.as_view(), name="Get and Post operations on season"),
-    path("<slug:season_id>", views.IndexOperations.as_view(), name="Put and Delete operations on season"),
+    path("<uuid:season_id>", views.IndexOperations.as_view(), name="Put and Delete operations on season"),
 ]
